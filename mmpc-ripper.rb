bcf7e1eaeae08573f431980c6a943c857737d145
Adam Louis <adamlouis@gmail.com>
2015-09-02 16:40:33 -0700

    refactor to use soundcloud gem

--- mmpc-ripper.rb
+++ mmpc-ripper.rb
@@ -1,25 +1,13 @@
 require_relative 'secrets'
-require 'open-uri'
-require 'json'
+require 'soundcloud'
 
-API_URL   = 'https://api.soundcloud.com'
 USER_URL  = 'https://soundcloud.com/themonday-morning-podcast'
 MAX_LIMIT = 200
 CLIENT_ID = Secrets::Soundcloud::CLIENT_ID
 
-
-
-def querystringify(hash)
-  hash.map{|k,v| "#{k}=#{v}"}.join('&')
-end
-
-def api(path, options = {})
-  options.merge!(client_id: CLIENT_ID)
-  JSON.parse(open("#{API_URL}#{path}?#{querystringify(options)}").read)
-end
-
 def get_page(path, options, page)
-  api(path, options.merge(limit: MAX_LIMIT, offset: page * MAX_LIMIT))
+  client = SoundCloud.new(client_id: CLIENT_ID)
+  client.get(path, options.merge(limit: MAX_LIMIT, offset: page * MAX_LIMIT))
 end
 
 def concatenate_paginated(path, options = {})
@@ -31,6 +19,7 @@ def concatenate_paginated(path, options = {})
   results
 end
 
-user = api('/resolve', url: USER_URL)
-tracks = concatenate_paginated("/users/#{user['id']}/tracks")
+client = SoundCloud.new(client_id: CLIENT_ID)
+user = client.get('/resolve', url: USER_URL)
+tracks = concatenate_paginated("/users/#{user.id}/tracks")
 p user['track_count'], tracks.size
