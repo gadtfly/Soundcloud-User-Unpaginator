cb67002f9c8a527487ef458105a56adc4c7c9806
Adam Louis <adamlouis@gmail.com>
2015-09-03 11:43:48 -0700

    add sorting

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'soundcloud'
 gem 'sinatra'
+gem 'haml'
--- app.rb
+++ app.rb
@@ -10,26 +10,28 @@ end
 __END__
 
 @@layout
+!!! 5
 %html
+  %head
+    %link{rel: :stylesheet, href: 'https://cdnjs.cloudflare.com/ajax/libs/sortable/0.6.0/css/sortable-theme-minimal.css'}
+    %script{src: 'https://cdnjs.cloudflare.com/ajax/libs/sortable/0.6.0/js/sortable.min.js'}
   %body
     %form{method: :get}
       %input{name: :url}
     = yield
 
 @@main
-%table
+%table{data: {sortable: true}}
   %thead
     %tr
-      %th id
-      %th title
-      %th description
+      %th Title
+      %th{data: {sortable: 'false'}} Description
       - @tracks.first.counts.keys.each do |count_key|
-        %th= count_key
+        %th= count_key.capitalize
   %tbody
     - @tracks.each do |track| 
       %tr
-        %td= track.id
-        %td
+        %td{data: {value: track.id}}
           %a{href: track.authorized_stream_url, download: true}= track.title
         %td= track.description
         - track.counts.values.each do |count_value|
